c40d7c10f760424990e24b9896a697e795629107
Itooonn <karlmahusay2@gmail.com>
2025-12-08 20:09:06 +0800

    update logout redirect to index.php

--- src/logout.php
+++ src/logout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header('Location: login.php');
+// Redirect to index.php located one level above this `src/` folder
+header('Location: ../index.php');
 exit;
